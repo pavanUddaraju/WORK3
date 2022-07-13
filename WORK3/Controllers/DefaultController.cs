e5ec532c367e24ba2ef28bd2f488ba89ce0dff96
pavanUddaraju <pavanuddaraju1234@gmail.com>
2022-07-13 21:11:35 +0530

    Add project files.

---
+++ WORK3/Controllers/DefaultController.cs
@@ -0,0 +1,110 @@
+﻿
+using System;
+using WORK3.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WORK3.ViewModels;
+
+namespace WORK3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Register registerDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var databaseContext = new pavanEntities2())
+                {
+
+                    Form rg = new Form();
+
+
+                    rg.FirstName = registerDetails.FirstName;
+                    rg.LastName = registerDetails.LastName;
+                    rg.PhNum = registerDetails.PhNum;
+                    rg.Email = registerDetails.Email;
+                    rg.Password = registerDetails.Password;
+
+
+                    databaseContext.Forms.Add(rg);
+                    databaseContext.SaveChanges();
+                }
+
+                ViewBag.Message = "User Details Saved";
+                return View("Register");
+            }
+            else
+            {
+
+                return View("Register", registerDetails);
+            }
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var isValidUser = IsValidUser(model);
+
+                if (isValidUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, false);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("Failure", "Wrong Username and password combination !");
+                    return View();
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public Form IsValidUser(Login model)
+        {
+            using (var dataContext = new pavanEntities2())
+            {
+                Form user = dataContext.Forms.Where(query => query.Email.Equals(model.Email) && query.Password.Equals(model.Password)).SingleOrDefault();
+                if (user == null)
+                    return null;
+                else
+                    return user;
+            }
+        }
+
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
+    
---
+++ WORK3/ViewModels/Register.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WORK3.ViewModels
+{
+    public class Register
+    {
+        [Key]
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Mobile Number")]
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Please Enter Valid Mobile Number.")]
+        public int PhNum { get; set; }
+
+
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail id is not valid")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
